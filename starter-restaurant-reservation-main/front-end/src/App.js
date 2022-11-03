f2ec4f82f43b5dc569cc4ea97956820a70d5e450
Aaron <ajazzar@mail.usf.edu>
2022-11-03 01:55:47 -0400

    updated front-end

--- starter-restaurant-reservation-main/front-end/src/App.js
+++ starter-restaurant-reservation-main/front-end/src/App.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "./layout/Layout";
-
+import ReservationForm from "./reservations/new";
 /**
  * Defines the root application component.
  * @returns {JSX.Element}
  */
 function App() {
   return (
-    <Switch>
-      <Route path="/">
-        <Layout />
-      </Route>
-    </Switch>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Layout />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route path="/reservations/new">
+          <ReservationForm />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
