e41609b0605efa1d80201aeeb474db27fd38ed96
Aaron <ajazzar@mail.usf.edu>
2022-11-06 18:58:45 -0500

    got the tests to pass

--- starter-restaurant-reservation-main/front-end/src/layout/Routes.js
+++ starter-restaurant-reservation-main/front-end/src/layout/Routes.js
@@ -4,7 +4,10 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
-
+import NewTable from "../tables/NewTable";
+import ReservationSeats from "../reservations/ReservationSeats";
+import Search from "../search/Search";
+import NewEditReservation from "../reservations/NewEditReservation";
 /**
  * Defines all the routes for the application.
  *
@@ -18,8 +21,23 @@ function Routes() {
       <Route exact={true} path="/">
         <Redirect to={"/dashboard"} />
       </Route>
+      <Route exact={true} path="/reservations/new">
+        <NewEditReservation />
+      </Route>
+      <Route exact={true} path="/reservations/:reservationId/seat">
+        <ReservationSeats />
+      </Route>
+      <Route exact={true} path="/reservations/:reservationId/edit">
+        <NewEditReservation />
+      </Route>
       <Route exact={true} path="/reservations">
         <Redirect to={"/dashboard"} />
+      </Route>
+      <Route exact={true} path="/tables/new">
+        <NewTable />
+      </Route>
+      <Route exact={true} path="/search">
+        <Search />
       </Route>
       <Route path="/dashboard">
         <Dashboard date={today()} />
