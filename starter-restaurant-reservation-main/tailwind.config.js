cd2cdf064a9a93133dfe48964a3e206478c7d418
Aaron <ajazzar@mail.usf.edu>
2022-11-06 20:44:26 -0500

    tests are passing

---
+++ starter-restaurant-reservation-main/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./front-end/src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    dropShadow: {
+      "3xl": "0 10px 10px rgba(0, 0, 0, 0.35)",
+    },
+  },
+  plugins: [],
+};
