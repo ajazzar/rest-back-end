2a88770ece245f65b7a813546a4b3e75567ffc37
Aaron <63377965+ajazzar@users.noreply.github.com>
2022-11-09 01:39:54 -0500

    Update Layout.js

--- starter-restaurant-reservation-main/front-end/src/layout/Layout.js
+++ starter-restaurant-reservation-main/front-end/src/layout/Layout.js
@@ -32,7 +32,7 @@ function Layout() {
           </div>
         </Link>
         <button type="button" onClick={() => handleSidebar()} className="p-4 focus:outline-none focus:bg-gray-700">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
