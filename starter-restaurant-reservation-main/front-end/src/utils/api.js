94abd63c65d3debddfdad40941fe6d07c13be4e6
Aaron <ajazzar@mail.usf.edu>
2022-11-08 22:01:40 -0500

    Update api.js

--- starter-restaurant-reservation-main/front-end/src/utils/api.js
+++ starter-restaurant-reservation-main/front-end/src/utils/api.js
@@ -7,7 +7,7 @@ import formatReservationTime from "./format-reservation-date";
 
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  "https://restaurant-reservations-lg39.vercel.app";
+  "postgres://qvxtcbuj:AJUc5Kd8dPv5-oJqR1zMKwKPOmZK6hrA@jelani.db.elephantsql.com/qvxtcbuj";
 /**
  * Defines the default headers for these functions to work with `json-server`
  */
