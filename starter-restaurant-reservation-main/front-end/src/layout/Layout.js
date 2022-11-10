4d65a0a929aebc4e415897c1e638d31e5f7a7737
Aaron <ajazzar@mail.usf.edu>
2022-11-10 08:39:10 -0500

    Update Layout.js

--- starter-restaurant-reservation-main/front-end/src/layout/Layout.js
+++ starter-restaurant-reservation-main/front-end/src/layout/Layout.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Menu from "./Menu";
 import Routes from "./Routes";
-
 import "./Layout.css";
-
 /**
  * Defines the main layout of the application.
  *
@@ -11,16 +9,30 @@ import "./Layout.css";
  *
  * @returns {JSX.Element}
  */
+
+function handleSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.classList.toggle("-translate-x-full");
+}
+
 function Layout() {
   return (
     <div className="container-fluid">
+      {/* mobile menu bar */}
       <div className="row h-100">
-        <div className="col-md-2 side-bar">
-          <Menu />
-        </div>
-        <div className="col">
-          <Routes />
-        </div>
+        <button
+          type="button"
+          onClick={() => handleSidebar()}
+          className="p-4 focus:outline-none focus:bg-gray-700"
+        ></button>
+      </div>
+      {/* sidebar */}
+      <div className="col-md-2 side-bar">
+        <Menu />
+      </div>
+      {/* main content */}
+      <div className="col">
+        <Routes />
       </div>
     </div>
   );
