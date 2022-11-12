e224e159612edf00d1a84ad3ec7d4ee1ca7e0d17
Aaron <ajazzar@mail.usf.edu>
2022-11-11 21:29:40 -0500

    update

--- starter-restaurant-reservation-main/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ["./front-end/src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    dropShadow: {
-      "3xl": "0 10px 10px rgba(0, 0, 0, 0.35)",
-    },
-  },
-  plugins: [],
-};
