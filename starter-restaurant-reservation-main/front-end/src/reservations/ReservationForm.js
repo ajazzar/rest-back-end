4e7861beccd88f154b139c692f774f10b5eee490
Aaron <ajazzar@mail.usf.edu>
2022-11-19 22:36:37 -0500

    update

--- starter-restaurant-reservation-main/front-end/src/reservations/ReservationForm.js
+++ starter-restaurant-reservation-main/front-end/src/reservations/ReservationForm.js
@@ -44,12 +44,11 @@ export default function ReservationForm({
         <div className="form-group">
           <label htmlFor="mobile_number">Mobile Number</label>
           <input
-            type="tel"
+            type="number"
             className="form-control"
             id="mobile_number"
             name="mobile_number"
             placeholder="123-456-7890"
-            pattern="\d{3}-\d{3}-\d{4}"
             value={formData.mobile_number}
             onChange={handleChange}
             required
