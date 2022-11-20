f50f781b8d125b86d40d8016ea77a3e85e0a5a09
Aaron <63377965+ajazzar@users.noreply.github.com>
2022-11-20 12:49:57 -0500

    Update ReservationForm.js

--- starter-restaurant-reservation-main/front-end/src/reservations/ReservationForm.js
+++ starter-restaurant-reservation-main/front-end/src/reservations/ReservationForm.js
@@ -1,9 +1,5 @@
 import React from "react";
 const dayjs = require("dayjs");
-const utc = require("dayjs/plugin/utc");
-const timezone = require("dayjs/plugin/timezone");
-dayjs.extend(utc);
-dayjs.extend(timezone);
 
 export default function ReservationForm({
   handleSubmit,
