4a8d9624c97d3eee2e2714f42d14cc9557f3544b
Aaron <63377965+ajazzar@users.noreply.github.com>
2022-11-21 11:39:12 -0500

    Update ErrorAlert.js

--- starter-restaurant-reservation-main/front-end/src/layout/ErrorAlert.js
+++ starter-restaurant-reservation-main/front-end/src/layout/ErrorAlert.js
@@ -10,7 +10,7 @@ import React from "react";
 
 function ErrorAlert({ error }) {
   return (
-    {error && (
+    {{error} && (
       <div className="alert alert-danger m-2">Error: {error.message}</div>
     )}
   );
