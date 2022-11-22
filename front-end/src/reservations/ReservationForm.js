e5d9cb48a594397a6083df2d6de46a7f2704af6f
Aaron <ajazzar@mail.usf.edu>
2022-11-22 18:39:09 -0500

    Update ReservationForm.js

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -62,7 +62,7 @@ export default function ReservationForm({
             id="reservation_date"
             name="reservation_date"
             value={date}
-            // min={new Date().toISOString().split("T")[0]}
+            min={new Date().toISOString().split("T")[0]}
             onChange={handleChange}
             required
           />
